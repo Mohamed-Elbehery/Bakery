aab6e52e7fe04b072e0adec778bec3da20354a35
El-Behery <dev.elbehery@gmail.com>
2023-02-19 16:40:10 +0200

    done

--- src/main.js
+++ src/main.js
@@ -15,10 +15,7 @@ xMark.addEventListener('click', () => {
 })
 
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if(entry.isIntersecting) entry.target.classList.add('show');
-    else entry.target.classList.remove('show');
-  })
+  entries.forEach((entry) => entry.isIntersecting ? entry.target.classList.add('show') : entry.target.classList.remove('show'))
 })
 
 document.querySelectorAll('.hidden').forEach(el => observer.observe(el));
